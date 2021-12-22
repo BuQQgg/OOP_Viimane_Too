e8068400253f967455ac3bdc90cd5a89f3d428ae
BuQQgg <94952921+BuQQgg@users.noreply.github.com>
2021-12-22 13:05:35 +0200

    commit to branch

--- OOP viimane töö.py
+++ OOP viimane töö.py
@@ -4,25 +4,31 @@ Mäng
 Kevin Kiisk
 '''
 
-from easygui import
+
 
 class Karakter:
-     def __init__(self, nimi, elud, tugevus):
+    
+    def __init__(self, nimi, elud, tugevus):
         self.nimi = nimi
         self.elud = elud
         self.tugevus = tugevus
 
-    def kaotaElusi(self, elup):
-        self.elud = self.elud - elup
+    def kaotaElusi(self, kaotaElud):
+        self.elud = self.elud - kaotaElud
 
 class Vaenlane:
+    
     def __init__(self, elud, tugevus):
         self.elud = elud
         self.tugevus = tugevus
 
-    def kaotaElusi(self, elud):
-        self.elud = self.elud - elud
+    def kaotaElusi(self, kaotaElud):
+        self.elud = self.elud - kaotaElud
             
+from easygui import *    
             
-            
-            
+nimi = enterbox("Tere kuidas on teie nimi?")
+if nimi == None:
+    msgbox("Palun sisesta korrektne nimi!!")
+else:
+    msgbox("Tere, " + nimi +"! " + "Olete sattunud labürinti. Teil on 100 elu, kui satute kokku vaenlastega kaotad 50 elu aga kui teed õiged valikud jõuad õnnelikult lõppu.")
